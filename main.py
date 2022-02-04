84a1c05066a4e64be7429ed9a4cd03127762d0d1
CyberRor <87041881+Rordan1@users.noreply.github.com>
2022-02-04 16:19:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#here we go
+
+name = "Rordan"
+
+age = 23
+
+actual_age = 23.75
+
+print(name, age);
+
+print (type(name))
+
+print (type(age))
+
+print (type(actual_age))
+
+print (7+6 * 9)
+
+print (15*8-9)
+
+print (8-9*15)
+
+math = 17 * 2
+
+math2 = 17 ** 2
+
+print(math)
+print (math2)
+
+print(17*17)
+
+results = age + actual_age + math + math2
+
+print(results)
