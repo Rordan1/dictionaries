350c0888eac840ed0732ca5d7ee1e0d281aca069
CyberRor <87041881+Rordan1@users.noreply.github.com>
2022-02-07 17:28:03 -0500

    Created a working script
    Allows for translation of english months to french months

---
+++ dictionary2.0.py
@@ -0,0 +1,20 @@
+EnglishMonths_to_FrenchMonths = {
+    "January": "Janvier",
+    "February": " Février",
+    "March": " Mars",
+    "April": " Avril",
+    "May": " Mai",
+    "June": " Juin",
+    "July": " Juillet",
+    "August": " Août",
+    "September": " Septembre",
+    "November": " Novembre",
+    "December": " Décembre",
+}
+
+    month = input("Type the name of a month with the first letter as a capitol \n")
+
+    print(EnglishMonths_to_FrenchMonths[month])
+
+    input()
+
