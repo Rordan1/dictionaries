edfaf21438efc50f74a4d0b153d5cbf3159ca6e0
Rordan1 <87041881+Rordan1@users.noreply.github.com>
2022-09-09 23:05:16 -0400

    small changes

---
+++ dictionary3.0.py
@@ -0,0 +1,18 @@
+EnglishMonths_to_FrenchMonths = {
+    "January": "Janvier",
+    "February": "Février",
+    "March": "Mars",
+    "April": "Avril",
+    "May": "Mai",
+    "June": "Juin",
+    "July": "Juillet",
+    "August": "Août",
+    "September": "Septembre",
+    "October": "Octobre",
+    "November": "Novembre",
+    "December": "Décembre",
+}
+
+month = input("Type the name of a month with the first letter as a capitol: \n")
+
+print(EnglishMonths_to_FrenchMonths[month])
